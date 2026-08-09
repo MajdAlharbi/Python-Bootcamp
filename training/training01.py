0aedbc6dca2b4d8aa8e3d143b219d1d422f8f724
MAJD <majd9alharbi@gmail.com>
2026-08-09 17:54:12 +0300

    add week3 folder

---
+++ training/training01.py
@@ -0,0 +1,13 @@
+# --------------
+# iformtion about file
+# liciss
+# who creat the file
+# why created the file
+# write beside the programming line
+# write before the programming line
+# --------------
+
+print("print")
+
+num = "majd"
+age = 55
