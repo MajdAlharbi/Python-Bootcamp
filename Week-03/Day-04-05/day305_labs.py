0e5633192ddc47f3019e39d853f0d67784aadf98
MAJD <majd9alharbi@gmail.com>
2026-08-15 03:15:22 +0300

    Finalize Week 3 Days 4-5 folder merge

--- Week-03/Day-04-05/day305_labs.py
+++ Week-03/Day-04-05/day305_labs.py
@@ -47,6 +47,7 @@ flattened_list = []
 for row in nested_list:
     for column in row:
         flattened_list.append(column)
+
     print(flattened_list)
     comp_flattened_list = [column for row in nested_list for column in row]
     print(comp_flattened_list)
