0af5ddadb13204b3ffcc8030653af82e32f3edae
MAJD <majd9alharbi@gmail.com>
2026-08-31 18:10:25 +0300

    Week-06 Day-02:Lab:multi-page Django starer site

---
+++ Week-06/Day-02/mysite/core/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def home(request):
+    return HttpResponse("Welcome to Home")
+
+
+def about(request):
+    return HttpResponse("Welcome to About")
+
+
+def contact(request):
+    return HttpResponse("Welcome to Contact")
